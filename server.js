4dd3f999bb04fd8cb286487463fbd26369f4ebdc
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-01-12 12:46:53 -0300

    Installing mongoose.

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-
+// STARTING  APP
 const app = express();
+
+// STARTING DB
+mongoose.connect('mongodb://localhost:27017/nodeinit',
+                 {useNewUrlParser: true}
+);
+
+
+// CREATING ROUTE
 app.get('/', (req, res) => {
     res.send('Hello Rocketseat');
 });
