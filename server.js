c8e864ed592670d0e1440e54aca24ce9c2422bae
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-01-12 12:59:29 -0300

    Creating model product

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // STARTING  APP
 const app = express();
@@ -9,10 +10,19 @@ mongoose.connect('mongodb://localhost:27017/nodeinit',
                  {useNewUrlParser: true}
 );
 
+requireDir('src/models');
+
+const product = mongoose.model('Product');
+
 
 // CREATING ROUTE
 app.get('/', (req, res) => {
-    res.send('Hello Rocketseat');
+    product.create({
+        title: 'React Native',
+        description: 'Build Native app with reactive',
+        url: 'https://github.com/facebook/react-native'
+    })
+    return res.send('Hello Rocketseat');
 });
 
 
