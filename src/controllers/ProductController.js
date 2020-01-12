7dfa96c3a26000804ecdfd8d6b111602a1c1cf9c
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-01-12 14:30:27 -0300

    Crud Operations.

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -6,8 +6,20 @@ module.exports = {
         const products = await product.find();
         return res.json(products);
     },
+    async show(req, res) {
+        const productReturned = await product.findById(req.params.id);
+        return res.json(productReturned);
+    },
     async store(req, res) {
         const productSaved = await product.create(req.body);
         return res.json(productSaved);
-    }
+    },
+    async update(req, res) {
+        const productSaved = await product.findByIdAndUpdate(req.params.id, req.body, {new: true}); // new true means that mongoose will return the row updated. if is false, mongoose will return row before was updated.
+        return res.json(productSaved);
+    },
+    async destroy(req, res) {
+        await product.findByIdAndDelete(req.params.id);
+        return res.send();
+    },
 };
