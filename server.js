284f7e61e540781c81519174ca4645a52f4476be
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-01-12 14:39:19 -0300

    Adding cors support.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // STARTING  APP
 const app = express();
 app.use(express.json()); // ALLOW TO SEND JSON OBJECTS.
+app.use(cors());
 
 
 
